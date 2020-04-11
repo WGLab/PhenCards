d319874a7b9f235dc54eeab03255b47422221e61
shawnxd <shawnxd@alumni.upenn.edu>
2020-04-11 13:55:03 -0400

    fixed ID search using the new output format with hyperlinks

--- app.py
+++ app.py
@@ -53,12 +53,16 @@ def get_results(phen_name: str, weight_model='pn'):
 
     # (1) if search by hpo id
     if weight_model == 'hpo':
-        if phen_name == None:
-            return "No input detected."
+        if not phen_name.isdigit():
+            phen_name = '0'
         # use phen_dict as final result
         phen_dict1 = defaultdict(list)
         phen_dict2 = defaultdict(list)
-        cursor1 = c1.execute("SELECT * FROM PHENBASE WHERE [HPO-ID]='" + phen_name + "'")
+        phen_dict2_OMIM = defaultdict(list)
+        phen_dict2_DECIPHER = defaultdict(list)
+        phen_dict2_ORPHA = defaultdict(list)
+        phen_dict3 = defaultdict(list)
+        cursor1 = c1.execute("SELECT * FROM PHENBASE WHERE [HPO-ID] LIKE'%" + phen_name + "%'")
         for row in cursor1:
             # index in database
             idx = row[0]
@@ -66,13 +70,29 @@ def get_results(phen_name: str, weight_model='pn'):
             OMIMID = row[2]
             HPOId = row[3]
             HPOName = row[4]
-            phen_dict1[idx].extend([HPOId, HPOName, phenName, OMIMID])
-            phen_dict2[idx].extend([phenName, OMIMID])
+            phen_dict1[idx].extend([phenName, HPOId, HPOName])
+            if OMIMID[:4] == 'OMIM':
+                phen_dict2_OMIM[idx].extend([phenName, OMIMID, HPOId, HPOName])
+            elif OMIMID[:8] == 'DECIPHER':
+                phen_dict2_DECIPHER[idx].extend([phenName, OMIMID, HPOId, HPOName])
+            elif OMIMID[:5] == 'ORPHA':
+                phen_dict2_ORPHA[idx].extend([phenName, OMIMID, HPOId, HPOName])
+            phen_dict2[idx].extend([phenName, OMIMID, HPOId, HPOName])
         # print(phen_dict)
-
+        cursor2 = c2.execute("SELECT * FROM ICD10BASE WHERE [ICD10-ID] LIKE'%" + phen_name + "%'")
+        for row in cursor2:
+            # index in database
+            idx = row[0]
+            ICD10ID = row[1]
+            PARENTIDX = row[2]
+            ABBREV = row[3]
+            NAME = row[4]
+            # output the JSON
+            phen_dict3[idx].extend([ICD10ID, PARENTIDX, ABBREV, NAME])
         # output the JSON
-        return format_json_table(weight_model.lower(), phen_dict1, 'HPO'), format_json_table(weight_model.lower(),
-                                                                                             phen_dict1, 'OMIM')
+        return format_json_table(weight_model.lower(), phen_dict1, 'HPO'), format_json_table(weight_model.lower(), phen_dict2_OMIM, 'OMIM'), \
+               format_json_table(weight_model.lower(), phen_dict2_DECIPHER, 'DECIPHER'), format_json_table(weight_model.lower(), phen_dict2_ORPHA, 'ORPHA'), \
+               format_json_table(weight_model.lower(), phen_dict3, 'ICD')
 
     # If no phenotype name available, exit the scripts.
     if phen_name is None:
@@ -131,10 +151,8 @@ def get_results(phen_name: str, weight_model='pn'):
         phen_dict3[idx].extend([ICD10ID, PARENTIDX, ABBREV, NAME])
 
     # return results in json file, transfer dict into json format
-    return format_json_table(weight_model.lower(), phen_dict1, 'HPO'), format_json_table(weight_model.lower(),
-                                                                                         phen_dict2_OMIM, 'OMIM'), \
-           format_json_table(weight_model.lower(), phen_dict2_DECIPHER, 'DECIPHER'), format_json_table(
-        weight_model.lower(), phen_dict2_ORPHA, 'ORPHA'), \
+    return format_json_table(weight_model.lower(), phen_dict1, 'HPO'), format_json_table(weight_model.lower(), phen_dict2_OMIM, 'OMIM'), \
+           format_json_table(weight_model.lower(), phen_dict2_DECIPHER, 'DECIPHER'), format_json_table(weight_model.lower(), phen_dict2_ORPHA, 'ORPHA'), \
            format_json_table(weight_model.lower(), phen_dict3, 'ICD')
 
 
@@ -304,6 +322,34 @@ def results_page():
         html_res = '</td>'.join(html_lst)
         return html_res
 
+    # https://bioportal.bioontology.org/search?q=DiGeorge%20Syndrome&ontologies=ORDO&include_properties=false&include_views=false&includeObsolete=false&require_definition=false&exact_match=false&categories=
+    def add_link_2OR(html_res):
+        html_lst = html_res.split("</td>")
+        for i in range(len(html_lst)):
+            item = html_lst[i]
+
+            if "<td>HP:" in item:
+                # find index of the <td>
+                idx = item.find("<td>HP:")
+                html_lst[i] = item[:(idx + 4)] + '<a href=https://hpo.jax.org/app/browse/search?q=' + item[(idx + 4):] + "&navFilter=all>" \
+                + item[(idx + 4):] + '</a>'
+
+            elif "<td>OMIM:" in item:
+                idx = item.find("<td>OMIM:")
+                html_lst[i] = item[:(idx + 4)] + '<a href=https://www.omim.org/search/?index=entry&start=1&limit=10&sort=score+desc%2C+prefix_sort+desc&search=' \
+                + item[(idx+9):] + ">" + item[(idx + 4):] + '</a>'
+
+            elif "<td>" in item:
+                idx = item.find("<td>")
+                if item[idx+4] in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" and item[idx+5:].isdigit():
+                    html_lst[i] = item[:(idx + 4)] + '<a href=https://www.icd10data.com/search?s=' + item[idx+4:] \
+                    + "&codebook=icd10all>" + item[(idx + 4):] + '</a>'
+                    continue
+                html_lst[i] = item[:(idx + 4)] + '<a href=https://bioportal.bioontology.org/search?q=' + item[(idx + 4):].replace(' ', '%20') \
+                + '&ontologies=ORDO&include_properties=false&include_views=false&includeObsolete=false&require_definition=false&exact_match=false">' + item[(idx + 4):] + '</a>'
+        html_res = '</td>'.join(html_lst)
+        return html_res
+
     # <ICD link> https://www.icd10data.com/search?s=waterhouse-friderchsen%20syndrome&codebook=icd10all
     def add_link_3(html_res):
         html_lst = html_res.split("</td>")
@@ -373,7 +419,7 @@ def results_page():
     html_table2D = add_link_2D(html_table2D)
     html_table2OR = json2html.convert(json=top_100_2OR,
                                       table_attributes="id=\"results-table2OR\" class=\"table table-striped table-bordered table-sm\"")
-    html_table2OR = add_link_2D(html_table2OR)
+    html_table2OR = add_link_2OR(html_table2OR)
     html_table3 = json2html.convert(json=top_100_3,
                                     table_attributes="id=\"results-table3\" class=\"table table-striped table-bordered table-sm\"")
     html_table3 = add_link_3(html_table3)
--- lib/json.py
+++ lib/json.py
@@ -6,72 +6,72 @@ def format_json_table(weight_model, gene_dict, type):
     
     if len(gene_dict) > 0:
 
-        # when searching for ids
-        if weight_model == 'hpo':
-            rank = 1
+        # # when searching for ids
+        # if weight_model == 'hpo':
+        #     rank = 1
+        #     for gene_item in gene_dict.keys():
+        #         # phen_dict[idx].extend([phenName, HPOId, HPOName])
+        #         gene_info_dict = {
+        #             'HPO ID': gene_dict[gene_item][0],
+        #             'HPO Name': gene_dict[gene_item][1],
+        #             'Phenotype Name': gene_dict[gene_item][2],
+        #         }
+        #         gene_info_lst.append(gene_info_dict)
+        #         rank += 1
+        #
+        # # when searching for terms instead of ids
+        # else:
+        if type == 'HPO':
             for gene_item in gene_dict.keys():
+                # output_file.write(str(rank) + "\t" + gene_dict[gene_item][0] + "\t" + str(gene_item) + "\t" + str( np.round(gene_dict[gene_item][1] / highest_score, 6 ) ) + "\t" +gene_dict[gene_item][2] + "\n" )
                 # phen_dict[idx].extend([phenName, HPOId, HPOName])
                 gene_info_dict = {
-                    'HPO ID': gene_dict[gene_item][0],
-                    'HPO Name': gene_dict[gene_item][1],
-                    'Phenotype Name': gene_dict[gene_item][2],
+                    'Phenotype Name': gene_dict[gene_item][0],
+                    'HPO ID': gene_dict[gene_item][1],
+                    'HPO Name': gene_dict[gene_item][2]
                 }
                 gene_info_lst.append(gene_info_dict)
-                rank += 1
+        elif type == 'OMIM':
+            for gene_item in gene_dict.keys():
+                # phen_dict2[idx].extend([phenName, OMIMID, HPOId, HPOName])
+                gene_info_dict = {
+                    'Phenotype Name': gene_dict[gene_item][0],
+                    'OMIM ID': gene_dict[gene_item][1],
+                    'Relative HPO ID': gene_dict[gene_item][2],
+                    'HPO Name': gene_dict[gene_item][3]
+                }
+                gene_info_lst.append(gene_info_dict)
+        elif type == 'DECIPHER':
+            for gene_item in gene_dict.keys():
+                # phen_dict2[idx].extend([phenName, OMIMID, HPOId, HPOName])
+                gene_info_dict = {
+                    'Phenotype Name': gene_dict[gene_item][0],
+                    'DECIPHER ID': gene_dict[gene_item][1],
+                    'Relative HPO ID': gene_dict[gene_item][2],
+                    'HPO Name': gene_dict[gene_item][3]
+                }
+                gene_info_lst.append(gene_info_dict)
 
-        # when searching for terms instead of ids
-        else:
-            if type == 'HPO':
-                for gene_item in gene_dict.keys():
-                    # output_file.write(str(rank) + "\t" + gene_dict[gene_item][0] + "\t" + str(gene_item) + "\t" + str( np.round(gene_dict[gene_item][1] / highest_score, 6 ) ) + "\t" +gene_dict[gene_item][2] + "\n" )
-                    # phen_dict[idx].extend([phenName, HPOId, HPOName])
-                    gene_info_dict = {
-                        'Phenotype Name': gene_dict[gene_item][0],
-                        'HPO ID': gene_dict[gene_item][1],
-                        'HPO Name': gene_dict[gene_item][2]
-                    }
-                    gene_info_lst.append(gene_info_dict)
-            elif type == 'OMIM':
-                for gene_item in gene_dict.keys():
-                    # phen_dict2[idx].extend([phenName, OMIMID, HPOId, HPOName])
-                    gene_info_dict = {
-                        'Phenotype Name': gene_dict[gene_item][0],
-                        'OMIM ID': gene_dict[gene_item][1],
-                        'HPO ID': gene_dict[gene_item][2],
-                        'HPO Name': gene_dict[gene_item][3]
-                    }
-                    gene_info_lst.append(gene_info_dict)
-            elif type == 'DECIPHER':
-                for gene_item in gene_dict.keys():
-                    # phen_dict2[idx].extend([phenName, OMIMID, HPOId, HPOName])
-                    gene_info_dict = {
-                        'Phenotype Name': gene_dict[gene_item][0],
-                        'DECIPHER ID': gene_dict[gene_item][1],
-                        'HPO ID': gene_dict[gene_item][2],
-                        'HPO Name': gene_dict[gene_item][3]
-                    }
-                    gene_info_lst.append(gene_info_dict)
-
-            elif type == 'ORPHA':
-                for gene_item in gene_dict.keys():
-                    # phen_dict2[idx].extend([phenName, OMIMID, HPOId, HPOName])
-                    gene_info_dict = {
-                        'Phenotype Name': gene_dict[gene_item][0],
-                        'ORPHA ID': gene_dict[gene_item][1],
-                        'HPO ID': gene_dict[gene_item][2],
-                        'HPO Name': gene_dict[gene_item][3]
-                    }
-                    gene_info_lst.append(gene_info_dict)
-            elif type == 'ICD':
-                for gene_item in gene_dict.keys():
-                    # phen_dict3[idx].extend([ICD10ID, PARENTIDX, ABBREV, NAME])
-                    gene_info_dict = {
-                        'ICD-10 Name': gene_dict[gene_item][3],
-                        'ICD-10 ID': gene_dict[gene_item][0],
-                        'Abbreviation Name': gene_dict[gene_item][2],
-                        'Parent Index': gene_dict[gene_item][1]
-                    }
-                    gene_info_lst.append(gene_info_dict)
+        elif type == 'ORPHA':
+            for gene_item in gene_dict.keys():
+                # phen_dict2[idx].extend([phenName, OMIMID, HPOId, HPOName])
+                gene_info_dict = {
+                    'Phenotype Name': gene_dict[gene_item][0],
+                    'ORPHA ID': gene_dict[gene_item][1],
+                    'Relative HPO ID': gene_dict[gene_item][2],
+                    'HPO Name': gene_dict[gene_item][3]
+                }
+                gene_info_lst.append(gene_info_dict)
+        elif type == 'ICD':
+            for gene_item in gene_dict.keys():
+                # phen_dict3[idx].extend([ICD10ID, PARENTIDX, ABBREV, NAME])
+                gene_info_dict = {
+                    'ICD-10 Name': gene_dict[gene_item][3],
+                    'ICD-10 ID': gene_dict[gene_item][0],
+                    'Abbreviation Name': gene_dict[gene_item][2],
+                    'Parent Index': gene_dict[gene_item][1]
+                }
+                gene_info_lst.append(gene_info_dict)
                                     
     json_fmt = json.dumps(gene_info_lst)
 
