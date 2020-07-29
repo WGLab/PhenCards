af92293979447aa371f803ffda0893d2faf04b47
stormliucong <stormliucong@gmail.com>
2020-07-29 11:15:21 -0700

    add docker for elasticsearch features

---
+++ app_init.sh
@@ -0,0 +1,7 @@
+# while [[ "$(curl -s -o /dev/null -w '%{http_code}' elasticsearch:9200)" != "200" ]]; do sleep 1; done \
+# && python index_db.py \
+# && python app.py
+
+# skip db indexing.
+while [[ "$(curl -s -o /dev/null -w '%{http_code}' elasticsearch:9200)" != "200" ]]; do sleep 1; done \
+&& python app.py
--- index_db.py
+++ index_db.py
@@ -3,8 +3,12 @@ from elasticsearch import helpers
 import json
 import time, datetime
 import os
+from datetime import datetime
 
-es = Elasticsearch(['localhost:9200'], timeout=60, retry_on_timeout=True)
+time.sleep(30) # sleep until elastic is fully started.
+
+
+es = Elasticsearch(['elasticsearch:9200'], timeout=60, retry_on_timeout=True)
 
 def index_doid(INDEX_NAME='doid',path_to_txt='/media/database/DOID_data_result/DOID-DATA.txt'):
     request_body = {
@@ -146,7 +150,7 @@ def index_umls(INDEX_NAME='umls',path_to_txt='/media/database/UMLS-DATA.txt'):
                 if len(es_data) > 0:
                     helpers.bulk(es, es_data, stats_only=False)    
 
-def index_autosuggest(INDEX_NAME='autosuggest',path_to_txt='/media/database/ICD10_data_result/ICD10-DATA.txt'):
+def index_autosuggest(INDEX_NAME='autosuggest',path_to_icd10='./database/ICD10_data_result/ICD10-DATA.txt',path_to_phenotype='./database/phenotype_database.csv'):
     request_body = {
         "settings": {
             "analysis": {
@@ -189,7 +193,8 @@ def index_autosuggest(INDEX_NAME='autosuggest',path_to_txt='/media/database/ICD1
             es.indices.delete(index=INDEX_NAME, ignore=404)
             es.indices.create(index=INDEX_NAME, body=request_body)
             es_data = []
-            with open(path_to_txt, "r") as ftxt:
+            n_doc = 0
+            with open(path_to_icd10, "r") as ftxt:
                 a = ftxt.readlines()
                 data = {}
                 for i in a:
@@ -203,11 +208,54 @@ def index_autosuggest(INDEX_NAME='autosuggest',path_to_txt='/media/database/ICD1
                     data['NAMESUGGEST']['input'].extend(eles[4].split())
                     action = {"_index": INDEX_NAME, '_source': data}
                     es_data.append(action)
-                    if len(es_data) > 1000:
+                    if len(es_data) > 10000:
                         helpers.bulk(es, es_data, stats_only=False)
                         es_data = []
+                        print("now =" + str(datetime.now()) + ': indexed ' + str(10000*n_doc) + ' documents')
+                        n_doc += 1
+                
                 if len(es_data) > 0:
                     helpers.bulk(es, es_data, stats_only=False)   
+                    print("now =" + str(datetime.now()) + ': indexed ICD10 is completed!')
+                    
+            es_data = []
+            n_doc = 0
+            with open(path_to_phenotype, "r") as ftxt:
+                a = ftxt.readlines()
+                data = {}
+                for i in a:
+                    eles = i.split('\t')
+                    # Disease ID
+                    data = {} # init to avoid deep copy.
+                    data['ID'] = eles[2]
+                    data['NAME'] = eles[1]
+                    data['ABBR'] = ''
+                    data['NAMESUGGEST'] = {}
+                    data['NAMESUGGEST']['input'] = [eles[1]]
+                    data['NAMESUGGEST']['input'].extend(eles[1].split())
+                    action = {"_index": INDEX_NAME, '_source': data}
+                    es_data.append(action)
+                    # HPO ID
+                    data = {} # init to avoid deep copy.
+                    data['ID'] = eles[3]
+                    data['NAME'] = eles[4]
+                    data['ABBR'] = ''
+                    data['NAMESUGGEST'] = {}
+                    data['NAMESUGGEST']['input'] = [eles[1]]
+                    data['NAMESUGGEST']['input'].extend(eles[1].split())
+                    action = {"_index": INDEX_NAME, '_source': data}
+                    es_data.append(action)
+                    if len(es_data) > 10000:
+                        helpers.bulk(es, es_data, stats_only=False)
+                        es_data = []
+                        print("now =" + str(datetime.now()) + ': indexed ' + str(10000*n_doc) + ' documents')
+                        n_doc += 1
+
+                
+                if len(es_data) > 0:
+                    helpers.bulk(es, es_data, stats_only=False)
+                    print("now =" + str(datetime.now()) + ': indexed phenotype.db is completed!')
+   
 
 
 if __name__ == "__main__":
